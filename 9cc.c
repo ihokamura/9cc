9b91dd433676dadafeaee60774a1d46bbe90e9dd
ihokamura <ihokamura.contact@gmail.com>
2019-11-04 12:44:17 +0900

    introduce syntax tree

--- 9cc.c
+++ 9cc.c
@@ -20,19 +20,44 @@ typedef enum {
     TK_EOF,      // end of input
 } TokenKind;
 
-// token type
+// kind of node in AST(abstract syntax tree)
+typedef enum {
+    ND_ADD, // addition(+)
+    ND_SUB, // subtraction(-)
+    ND_MUL, // multiplication(*)
+    ND_DIV, // division(/)
+    ND_NUM, // integer
+} NodeKind;
+
+// structure for token
 typedef struct Token Token;
 struct Token {
     TokenKind kind; // kind of token
     Token *next;    // next input token
-    int val;        // value of token if its kind is TK_NUM
+    int val;        // value of token (only for TK_NUM)
     char *str;      // token string
 };
+
+// structure for node in AST
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; // kind of node
+    Node *lhs;     // left hand side
+    Node *rhs;     // right hand side
+    int val;       // value of node (only for ND_NUM)
+};
+
 
 // currently parsing token
 Token *token;
 // input expression
 char *user_input;
+
+
+// function prototype
+Node *expr(void);
+Node *mul(void);
+Node *primary(void);
 
 
 /*
@@ -111,26 +136,17 @@ int expect_number(void)
 
 
 /*
-check if the current token is the end of input
-*/
-bool at_eof(void)
-{
-    return (token->kind == TK_EOF);
-}
-
-
-/*
 make a new token and concatenate it to the current token
 */
-Token *new_token(TokenKind kind, Token *cur_token, char *str)
+Token *new_token(TokenKind kind, Token *cur_tok, char *str)
 {
-    Token *new_token = calloc(1, sizeof(Token));
+    Token *new_tok = calloc(1, sizeof(Token));
 
-    new_token->kind = kind;
-    new_token->str = str;
-    cur_token->next = new_token;
+    new_tok->kind = kind;
+    new_tok->str = str;
+    cur_tok->next = new_tok;
 
-    return new_token;
+    return new_tok;
 }
 
 
@@ -154,9 +170,17 @@ Token *tokenize(char *str)
         }
 
         // parse an operator
-        if((*str == '+') || (*str == '-'))
+        if(
+            (*str == '+') || 
+            (*str == '-') || 
+            (*str == '*') || 
+            (*str == '/') || 
+            (*str == '(') || 
+            (*str == ')')
+            )
         {
-            current = new_token(TK_RESERVED, current, str++);
+            current = new_token(TK_RESERVED, current, str);
+            str++;
             continue;
         }
 
@@ -179,6 +203,159 @@ Token *tokenize(char *str)
 
 
 /*
+make a new node for non-number
+*/
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+    Node *node = calloc(1, sizeof(Node));
+
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+
+    return node;
+}
+
+
+/*
+make a new node for number
+*/
+Node *new_node_num(int val)
+{
+    Node *node = calloc(1, sizeof(Node));
+
+    node->kind = ND_NUM;
+    node->val = val;
+
+    return node;
+}
+
+
+/*
+make an expression
+* expr = mul ("+" mul | "-" mul)*
+*/
+Node *expr(void)
+{
+    Node *node = mul();
+
+    // parse tokens while finding a term
+    while(true)
+    {
+        if(consume('+'))
+        {
+            node = new_node(ND_ADD, node, mul());
+        }
+        else if(consume('-'))
+        {
+            node = new_node(ND_SUB, node, mul());
+        }
+        else
+        {
+            return node;
+        }
+    }
+}
+
+
+/*
+make a term
+* mul = primary ("*" primary | "/" primary)*
+*/
+Node *mul(void)
+{
+    Node *node = primary();
+
+    // parse tokens while finding a primary
+    while(true)
+    {
+        if(consume('*'))
+        {
+            node = new_node(ND_MUL, node, mul());
+        }
+        else if(consume('/'))
+        {
+            node = new_node(ND_DIV, node, mul());
+        }
+        else
+        {
+            return node;
+        }
+    }
+}
+
+
+/*
+make a primary
+* primary = num | "(" expr ")"
+*/
+Node *primary(void)
+{
+    if(consume('('))
+    {
+        // expression in brackets
+        Node *node = expr();
+ 
+        expect(')');
+ 
+        return node;
+    }
+
+    // number
+    return new_node_num(expect_number());
+}
+
+
+/*
+generate assembler code which emulates stack machine
+*/
+void gen(const Node *node)
+{
+    if(node->kind == ND_NUM)
+    {
+        printf("  push %d\n", node->val);
+
+        return;
+    }
+
+    // compile LHS and RHS
+    gen(node->lhs);
+    gen(node->rhs);
+
+    // pop RHS to rdi and LHS to rax
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    // execute operation
+    switch(node->kind)
+    {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+
+        default:
+            break;
+    }
+
+    // push return value
+    printf("  push rax\n");
+}
+
+
+/*
 main function of 9cc
 */
 int main(int argc, char **argv)
@@ -190,9 +367,12 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    // save and tokenize input
+    // save input
     user_input = argv[1];
-    token = tokenize(argv[1]);
+    // parse input
+    token = tokenize(user_input);
+    // construct syntax tree
+    Node *root = expr();
 
     // use Intel syntax
     printf(".intel_syntax noprefix\n");
@@ -201,23 +381,11 @@ int main(int argc, char **argv)
     printf(".global _main\n");
     printf("_main:\n");
 
-    // The first token of the expression should be a number.
-    printf("  mov rax, %d\n", expect_number());
+    // output assembler according to the syntax tree
+    gen(root);
 
-    // output assembler code consuming token streams of the form `+ [number]` or `- [number]'
-    while(!at_eof())
-    {
-        // addition
-        if(consume('+'))
-        {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        // subtraction
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
+    // pop return value from stack
+    printf("  pop rax\n");
 
     // exit main function
     printf("  ret\n");
