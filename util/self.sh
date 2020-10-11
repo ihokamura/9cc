9180eeb37ece0d10c10b4e86cb77034d3b0714a2
ihokamura <ihokamura.contact@gmail.com>
2020-10-11 13:37:22 +0900

    add check of difference between generated codes in test framework

--- util/self.sh
+++ util/self.sh
@@ -3,10 +3,14 @@
 # variables
 SOURCE=$(pwd)/source
 WORKSPACE=$(pwd)/self
+TEST=$(pwd)/test
 STD_HEADER=std_header.h
 GEN1_BIN=$(pwd)/9cc
 GEN2_BIN=$WORKSPACE/9cc_gen2
 GEN3_BIN=$WORKSPACE/9cc_gen3
+GEN1_ASSEMBLY=$TEST/test_code.s
+GEN2_ASSEMBLY=$TEST/test_code_gen2.s
+GEN3_ASSEMBLY=$TEST/test_code_gen3.s
 
 
 # functions
@@ -25,9 +29,14 @@ compile()
     COMPILER=$1
     OUTPUT=$2
 
-    for file in $SOURCE/*.c; do
-        expand $(basename $file) $COMPILER
-    done
+    expand declaration.c $COMPILER
+    expand expression.c $COMPILER
+    expand generator.c $COMPILER
+    expand main.c $COMPILER
+    expand parser.c $COMPILER
+    expand statement.c $COMPILER
+    expand tokenizer.c $COMPILER
+    expand type.c $COMPILER
 
     gcc -Wall -g -static -o $OUTPUT $WORKSPACE/*.s
 }
@@ -37,8 +46,9 @@ test()
     COMPILER=$1
     ASSEMBLY=$2
     BINARY=$3
+    TEST_SCRIPT=$(pwd)/util/test.sh
 
-    bash ./util/test.sh $COMPILER $ASSEMBLY $BINARY
+    bash $TEST_SCRIPT $COMPILER $ASSEMBLY $BINARY
 }
 
 expand()
@@ -72,13 +82,33 @@ expand()
 
     cat $WORKSPACE/$STD_HEADER $WORKSPACE/$TMP2 > $WORKSPACE/$CFILE
 
-    $COMPILER $OPTION $WORKSPACE/$CFILE > $WORKSPACE/${1%.c}.s
+    if [ $(basename $COMPILER) = 'gcc' ]; then
+        # for debug
+        gcc -Wall -g -S -masm=intel -o $WORKSPACE/${1%.c}.s $WORKSPACE/$CFILE
+    else
+        $COMPILER $OPTION $WORKSPACE/$CFILE > $WORKSPACE/${1%.c}.s
+    fi
+}
+
+check_diff()
+{
+    FILE1=$1
+    FILE2=$2
+
+    # compare files and report whether they are equal or not.
+    diff $FILE1 $FILE2
+    if [ $? != 0 ]; then
+        echo $FILE1 is different from $FILE2.
+    fi
 }
 
 
 # self-compile and run test
 prepare
 compile $GEN1_BIN $GEN2_BIN
-test $GEN2_BIN test_code_gen2.s test_bin_9cc_gen2
+test $GEN2_BIN $(basename $GEN2_ASSEMBLY) $(basename $GEN2_BIN)
 compile $GEN2_BIN $GEN3_BIN
-test $GEN3_BIN test_code_gen3.s test_bin_9cc_gen3
+test $GEN3_BIN $(basename $GEN3_ASSEMBLY) $(basename $GEN3_BIN)
+
+# check difference of assembly codes
+check_diff $GEN2_ASSEMBLY $GEN3_ASSEMBLY
